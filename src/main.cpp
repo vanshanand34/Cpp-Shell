588e3eff1e6803dad19dcef8aec49a646f79acb0
Vansh Anand <anandvansh34@gmail.com>
2025-05-18 16:53:13 +0530

    feat: Implement echo command and added arguments to strip function for optional leading and trailing spaces removal

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <string>
 
-std::string strip(std::string &str, int n)
+std::string strip(std::string str, bool leading = true, bool trailing = true)
 {
+  // If leading is false, it works as an rstrip function
+  // If trailing is false, it works as an lstrip function
+  // And if both true, then is works as a general strip function
+
+  int n = str.size();
   int start = 0;
   int end = n - 1;
-  while (start < n && str[start] == ' ')
-    start++;
 
-  if (start == n - 1)
+  if (leading)
+  {
+    while (start < n && str[start] == ' ')
+      start++;
+  }
+
+  if (trailing)
+  {
+    while (end >= start && str[end] == ' ')
+      end--;
+  }
+
+  if (start > end)
     return "";
-
-  while (end >= 0 && str[end] == ' ')
-    end--;
 
   return str.substr(start, end - start + 1);
 }
@@ -24,20 +36,28 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-
   std::string input = "input";
 
   while (true)
   {
     std::cout << "$ ";
     std::getline(std::cin, input);
-    input = strip(input, input.size());
+    input = strip(input, true, false);
 
     if (input.size() == 0)
       break;
-    
-    if (input == "exit 0") 
+
+    if (input == "exit 0")
       return 0;
+
+    if (input == "echo")
+      continue;
+
+    if (input.substr(0, 5) == "echo ")
+    {
+      std::cout << input.substr(5) << std::endl;
+      continue;
+    }
 
     std::cout << input << ": command not found" << std::endl;
   }
