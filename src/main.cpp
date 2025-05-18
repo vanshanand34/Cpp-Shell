e7e1381915e69dbc4c9c45517ecb5f985491cf8e
Vansh Anand <anandvansh34@gmail.com>
2025-05-18 14:05:16 +0530

    feat: Created strip function to remove leading/trailing spaces from string and implement REPL (Read Evaluate Print Loop) task

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 #include <string>
 
-int main() {
+std::string strip(std::string &str, int n)
+{
+  int start = 0;
+  int end = n - 1;
+  while (start < n && str[start] == ' ')
+    start++;
+
+  if (start == n - 1)
+    return "";
+
+  while (end >= 0 && str[end] == ' ')
+    end--;
+
+  return str.substr(start, end - start + 1);
+}
+
+int main()
+{
+
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
+  std::string input = "input";
 
-  std::cout << input << ": command not found" << std::endl;
+  while (true)
+  {
+    std::cout << "$";
+    std::getline(std::cin, input);
+    input = strip(input, input.size());
+
+    if (input.size() == 0)
+      break;
+
+    std::cout << input << ": command not found" << std::endl;
+  }
+
   return 0;
 }
