346d9f3467f12da10b4359a2eb65a5e5e802b393
Vansh Anand <anandvansh34@gmail.com>
2025-05-20 17:29:40 +0530

    feat: Added check for executable files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
+#include <filesystem>
 #include <iostream>
 #include <string>
 
+namespace fs = std::filesystem;
+
 std::string strip(std::string str, bool leading = true, bool trailing = true)
 {
-  // If leading is false, it works as an rstrip function
-  // If trailing is false, it works as an lstrip function
-  // And if both true, then is works as a general strip function
 
   int n = str.size();
   int start = 0;
@@ -29,6 +29,19 @@ std::string strip(std::string str, bool leading = true, bool trailing = true)
   return str.substr(start, end - start + 1);
 }
 
+std::vector<std::string> split(std::string str, char delimiter)
+{
+  std::stringstream ss(str);
+  std::string item = "";
+  std::vector<std::string> tokens;
+
+  while (std::getline(ss, item, delimiter))
+  {
+    tokens.push_back(item);
+  }
+  return tokens;
+}
+
 std::string commandType(std::string str)
 {
   std::string command_list[] = {"type", "exit", "echo"};
@@ -40,12 +53,36 @@ std::string commandType(std::string str)
   return ": not found";
 }
 
+std::string findFileinDir(char *directory_paths, std::string input)
+{
+  std::string curr_path = "";
+  std::vector<std::string> paths = split(directory_paths, ':');
+
+  for (std::string path : paths)
+  {
+    std::cout << "path - " << path << std::endl;
+    for (const auto &entry : fs::directory_iterator(path))
+    {
+      std::cout << "entry: " << entry.path().string() << std::endl;
+      if (entry.path().stem() == input)
+      {
+        std::string parent_path = entry.path().parent_path().string();
+        std::string filename = entry.path().stem().string();
+        return parent_path + "\\" + filename;
+      }
+    }
+  }
+  return "";
+}
+
 int main()
 {
 
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  char *directory_paths = getenv("PATH");
 
   std::string input = "input";
 
@@ -64,15 +101,31 @@ int main()
     if (input == "echo")
       continue;
 
-    if (input.substr(0, 5) == "echo ")
+    if (input.starts_with("echo"))
     {
       std::cout << input.substr(5) << std::endl;
       continue;
     }
     if (input.substr(0, 5) == "type ")
     {
-      std::string command_type = commandType(input.substr(5));
-      std::cout << input.substr(5) << command_type << std::endl;
+      std::string command = input.substr(5);
+
+      // First check if it is a shell builtin
+      std::string command_type = commandType(command);
+      if (command_type != ": not found")
+      {
+        std::cout << command << command_type << std::endl;
+        continue;
+      }
+
+      std::string file_path = findFileinDir(directory_paths, input.substr(5));
+      if (file_path != "")
+      {
+        std::cout << command << " is " << file_path << std::endl;
+        continue;
+      }
+
+      std::cout << command << command_type << std::endl;
       continue;
     }
 
