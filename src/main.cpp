0d2e4943960d12d266c4c96ab61b6a697049d860
Vansh Anand <anandvansh34@gmail.com>
2025-05-22 16:20:47 +0530

    feat: Refactored code and added handling of pwd command

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,23 @@
+#include <cstdlib>
 #include <filesystem>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cstdlib>
 
 namespace fs = std::filesystem;
 
-std::string strip(std::string str, bool leading = true, bool trailing = true)
-{
+std::string strip(std::string str, bool leading = true, bool trailing = true) {
 
   int n = str.size();
   int start = 0;
   int end = n - 1;
 
-  if (leading)
-  {
+  if (leading) {
     while (start < n && str[start] == ' ')
       start++;
   }
 
-  if (trailing)
-  {
+  if (trailing) {
     while (end >= start && str[end] == ' ')
       end--;
   }
@@ -31,100 +28,79 @@ std::string strip(std::string str, bool leading = true, bool trailing = true)
   return str.substr(start, end - start + 1);
 }
 
-std::vector<std::string> split(std::string str, char delimiter)
-{
+std::vector<std::string> split(std::string str, char delimiter) {
   std::stringstream ss(str);
   std::string item = "";
   std::vector<std::string> tokens;
 
-  while (std::getline(ss, item, delimiter))
-  {
+  while (std::getline(ss, item, delimiter)) {
     tokens.push_back(item);
   }
   return tokens;
 }
 
-std::string commandType(std::string str)
-{
+bool is_shell_builtin(std::string str) {
   std::string command_list[] = {"type", "exit", "echo"};
-  for (std::string command : command_list)
-  {
+  for (std::string command : command_list) {
     if (command == str)
-      return " is a shell builtin";
+      return true;
   }
-  return ": not found";
+  return false;
 }
 
-std::string findFileinDir(char *directory_paths, std::string input)
-{
-  std::string curr_path = "";
-  std::vector<std::string> paths = split(directory_paths, ':');
+std::string get_file_path(char *directory_paths, std::string input) {
+  try {
+    std::string curr_path = "";
+    std::vector<std::string> paths = split(directory_paths, ':');
 
-  for (std::string path : paths)
-  {
-    for (const auto &entry : fs::directory_iterator(path))
-    {
-      if (entry.path().stem() == input)
-      {
-        std::string parent_path = entry.path().parent_path().string();
-        std::string filename = entry.path().stem().string();
-        return parent_path + "/" + filename;
+    for (std::string path : paths) {
+      for (const auto &entry : fs::directory_iterator(path)) {
+        if (entry.path().stem() == input) {
+          std::string parent_path = entry.path().parent_path().string();
+          std::string filename = entry.path().stem().string();
+          return parent_path + "/" + filename;
+        }
       }
     }
+  } catch (const std::exception &ex) {
+    std::cerr << ex.what() << std::endl;
   }
   return "";
 }
 
-void handleTypeCmd(std::string input, char *directory_paths)
-{
-  try
-  {
-    std::string command = input.substr(5);
+std::string get_type(std::string command, char *directory_paths) {
+  try {
+    // First check if command is a shell builtin
+    bool is_builtin = is_shell_builtin(command);
 
-    // First check if it is a shell builtin
-    std::string command_type = commandType(command);
+    if (is_builtin)
+      return "builtin";
 
-    if (command_type != ": not found")
-    {
-      std::cout << command << command_type << std::endl;
-      return;
-    }
+    std::string file_path = get_file_path(directory_paths, command);
+    return file_path == "" ? "invalid" : file_path;
 
-    std::string file_path = findFileinDir(directory_paths, input.substr(5));
-
-    if (file_path != "")
-    {
-      std::cout << command << " is " << file_path << std::endl;
-      return;
-    }
-
-    std::cout << command << command_type << std::endl;
-    return;
-  }
-  catch (const std::exception &e)
-  {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
   }
+
+  return "invalid";
 }
 
-int main()
-{
-  try
-  {
-
+int main() {
+  try {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
     char *directory_paths = getenv("PATH");
-
     std::string input = "input";
 
-    while (true)
-    {
+    while (true) {
       std::cout << "$ ";
       std::getline(std::cin, input);
       input = strip(input, true, false);
+      std::vector<std::string> tokens = split(input, ' ');
+      std::string cmd = tokens[0];
 
       if (input.empty())
         break;
@@ -132,39 +108,49 @@ int main()
       if (input == "exit 0")
         return 0;
 
-      if (input == "echo")
-        continue;
-
-      if (input.starts_with("echo"))
-      {
+      if (cmd == "echo") {
+        if (tokens.size() == 1)
+          continue;
         std::cout << input.substr(5) << std::endl;
         continue;
       }
 
-      if (input.substr(0, 5) == "type ")
-      {
-        handleTypeCmd(input, directory_paths);
+      if (cmd == "type") {
+
+        if (tokens.size() == 1)
+          continue;
+
+        std::string cmd_type = get_type(input.substr(5), directory_paths);
+
+        if (cmd_type == "builtin")
+          std::cout << input.substr(5) << " is a shell builtin" << std::endl;
+
+        else if (cmd_type == "invalid")
+          std::cout << cmd << ": not found" << std::endl;
+
+        else
+          std::cout << cmd << " is " << cmd_type << std::endl;
+
+        continue;
+      }
+
+      if (cmd == "pwd") {
+        std::cout << fs::current_path().string() << std::endl;
         continue;
       }
 
       // Check for executable files
-      std::vector<std::string> arguments = split(input, ' ');
-      std::string cmd = arguments[0];
-      std::string file_path = findFileinDir(directory_paths, cmd);
+      std::string file_path = get_file_path(directory_paths, cmd);
 
-      if (file_path != "")
-      {
+      if (file_path != "") {
         // Execute the file
-        int output = system((cmd + input.substr(cmd.size())).c_str());
-        // std::cout << output << std::endl;
+        int output = system(input.c_str());
         continue;
       }
 
       std::cout << input << ": command not found" << std::endl;
     }
-  }
-  catch (const std::exception &e)
-  {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
   }
 
