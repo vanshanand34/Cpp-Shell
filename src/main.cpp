e9c8e7fd32af76f11ea02da001720fcc3078b99c
Vansh Anand <anandvansh34@gmail.com>
2025-05-23 22:40:28 +0530

    feat: Handled single quotes and cat command

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <vector>
 #ifdef _WIN32
@@ -11,7 +13,7 @@
 
 namespace fs = std::filesystem;
 
-std::vector<std::string> split(std::string str, char delimiter) {
+std::vector<std::string> split_args(std::string str, char delimiter) {
   std::stringstream ss(str);
   std::string item = "";
   std::vector<std::string> tokens;
@@ -22,8 +24,45 @@ std::vector<std::string> split(std::string str, char delimiter) {
   return tokens;
 }
 
+std::string check_remove_quotes(std::string &token) {
+
+  if (token.size() > 1 && token[0] == '\'' && token[token.size() - 1] == '\'')
+    return token.substr(1, token.size() - 2);
+  return token;
+}
+
+std::vector<std::string> split_with_quotes(std::string str) {
+  std::vector<std::string> tokens;
+  std::string curr_token = "";
+
+  for (int i = 0; i < str.size(); i++) {
+    if ((str[i] == ' ' && curr_token[0] != '\'')) {
+
+      if (curr_token != "")
+        tokens.push_back(curr_token);
+
+      curr_token = "";
+      continue;
+    }
+    if (str[i] == '\'' && curr_token[0] == '\'') {
+
+      // curr_token = curr_token.substr(1, curr_token.size() - 1);
+      tokens.push_back(curr_token + '\'');
+      curr_token = "";
+      continue;
+    }
+
+    curr_token += str[i];
+  }
+
+  if (curr_token != "")
+    tokens.push_back(curr_token);
+
+  return tokens;
+}
+
 bool is_shell_builtin(std::string str) {
-  std::string command_list[] = {"pwd", "cd", "type", "exit", "echo"};
+  std::string command_list[] = {"pwd", "cd", "type", "exit", "echo", "cat"};
   for (std::string command : command_list) {
     if (command == str)
       return true;
@@ -34,7 +73,7 @@ bool is_shell_builtin(std::string str) {
 std::string get_file_path(char *directory_paths, std::string input) {
   try {
     std::string curr_path = "";
-    std::vector<std::string> paths = split(directory_paths, ':');
+    std::vector<std::string> paths = split_args(directory_paths, ':');
 
     for (std::string path : paths) {
       for (const auto &entry : fs::directory_iterator(path)) {
@@ -69,6 +108,29 @@ std::string get_type(std::string command, char *directory_paths) {
   return "invalid";
 }
 
+void exec_cat_cmd(std::vector<std::string> file_names) {
+  for (std::string &file_name : file_names) {
+
+    if (file_name == "cat")
+      continue;
+
+    std::string cmd = "cat " + file_name;
+
+    FILE *fp = _popen(cmd.c_str(), "r");
+
+    if (fp == NULL) {
+      std::cerr << "error opening file : " << file_name << std::endl;
+      continue;
+    }
+
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+      std::cout << buffer;
+    }
+    _pclose(fp);
+  }
+}
+
 int main() {
   try {
     // Flush after every std::cout / std:cerr
@@ -82,18 +144,32 @@ int main() {
     while (true) {
       std::cout << "$ ";
       std::getline(std::cin, input);
-      std::vector<std::string> tokens = split(input, ' ');
-      std::string cmd = tokens[0];
 
       if (input.empty())
-        break;
+        return 0;
+
+
+      std::vector<std::string> tokens = split_with_quotes(input);
+      std::string cmd = tokens[0];
 
       if (input == "exit 0")
         return 0;
 
       if (cmd == "echo") {
+
         if (tokens.size() == 1)
           continue;
+
+        if (input.contains('\'')) {
+
+          for (std::string token : tokens) {
+            if (token == "echo")
+              continue;
+            std::cout << check_remove_quotes(token) << " ";
+          }
+          std::cout << std::endl;
+          continue;
+        }
         std::cout << input.substr(5) << std::endl;
         continue;
       }
@@ -134,6 +210,11 @@ int main() {
           std::cout << "cd: " << destination_dir
                     << ": No such file or directory" << std::endl;
         }
+        continue;
+      }
+
+      if (cmd == "cat") {
+        exec_cat_cmd(tokens);
         continue;
       }
 
